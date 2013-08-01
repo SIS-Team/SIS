ffba72119bb9e23c2b56e5b3c6c5b0f4a02537f7
Marco <handle.marco@tele2.at>
2013-08-01 11:21:31 +0200

    update der select.php

--- modules/database/selects.php
+++ modules/database/selects.php
@@ -51,7 +51,7 @@ return mysql_query($sql);
 
 function selectSection($where,$order){
 
-$sql="SELECT sections.ID , sections.name as seName, teachers.short as teShort FROM sections INNER JOIN teachers ON sections.teacher=teachers.ID";
+$sql="SELECT sections.ID , sections.name as seName, teachers.short as teShort FROM sections INNER JOIN teachers ON sections.teacherFK=teachers.ID";
 
 if($where!="" && $order!=""){		//Wenn beide Variablen beide gesetzt
 	//echo "1";
@@ -82,7 +82,7 @@ return mysql_query($sqlex);
 
 function selectRooms($where,$order){
 
-$sql= "SELECT rooms.ID, rooms.name as roName, teachers.short as teShort FROM rooms INNER JOIN teachers ON rooms.teacher=teachers.ID";
+$sql= "SELECT rooms.ID, rooms.name as roName, teachers.short as teShort FROM rooms INNER JOIN teachers ON rooms.teacherFK=teachers.ID";
 
 //printf("%s,%s,%s",$table, $where, $order);
 if($where!="" && $order!=""){		//Wenn beide Variablen beide gesetzt
@@ -114,7 +114,7 @@ return mysql_query($sqlex);
 
 function selectTeacher($where,$order){
 
-$sql= "SELECT teachers.ID, teachers.name as teName, teachers.short as teShort, teachers.display, sections.name as seName FROM teachers INNER JOIN sections ON teachers.section=sections.ID";
+$sql= "SELECT teachers.ID, teachers.name as teName, teachers.short as teShort, teachers.display, sections.name as seName FROM teachers INNER JOIN sections ON teachers.sectionFK=sections.ID";
 
 //printf("%s,%s,%s",$table, $where, $order);
 if($where!="" && $order!=""){		//Wenn beide Variablen beide gesetzt
@@ -144,7 +144,7 @@ return mysql_query($sqlex);
 
 function selectClass($where,$order){
 
-$sql= "SELECT classes.ID, classes.name as clName, sections.name as seName, teachers.short as teShort, rooms.name as roName FROM classes INNER JOIN sections ON sections.ID=classes.section INNER JOIN teachers ON teachers.ID=classes.teacher INNER JOIN rooms ON rooms.ID=classes.class";
+$sql= "SELECT classes.ID, classes.name as clName, sections.name as seName, teachers.short as teShort, rooms.name as roName FROM classes INNER JOIN sections ON sections.ID=classes.sectionFK INNER JOIN teachers ON teachers.ID=classes.teacherFK INNER JOIN rooms ON rooms.ID=classes.classFK";
 
 //printf("%s,%s,%s",$table, $where, $order);
 if($where!="" && $order!=""){		//Wenn beide Variablen beide gesetzt
@@ -173,7 +173,7 @@ return mysql_query($sqlex);
 }
 
 
-function selectLesson($where,$order){
+function selectLesson($where,$order){	//TODO
 
 $sql= "SELECT lessons.ID, classes.name as clName, rooms.name as roName, teachers.short as teShort, subjects.short as suShort, days.short as daShort, lessons.hour, lessons.length FROM lessons INNER JOIN classes ON classes.ID=lessons.class INNER JOIN rooms ON rooms.ID=lessons.room INNER JOIN teachers ON teachers.ID=lessons.teachers INNER JOIN subjects ON subjects.ID=lessons.subject INNER JOIN days ON days.ID=lessons.day";
 
@@ -205,8 +205,7 @@ return mysql_query($sqlex);
 
 function selectMissingTeacher($where,$order){
 
-//$sql= "SELECT missingTeachers.ID, teachers.teShort, missingTeachers.sDay, hours.hour, missingTeachers.eDay, hours.hour, missingTeachers.sure, missingTeachers.reason FROM missingTeachers INNER JOIN teachers ON teacher.ID=missingTeachers.teacher INNER JOIN hours ON hours.ID=missingTeachers.sHour INNER JOIN hours ON hours.ID=missingTeachers.eHour";
-  $sql= "SELECT missingTeachers.ID, teachers.short as teShort, missingTeachers.sDay, missingTeachers.sHour, missingTeachers.eDay, missingTeachers.eHour, missingTeachers.sure, missingTeachers.reason FROM missingTeachers INNER JOIN teachers ON teachers.ID = missingTeachers.teacher";
+  $sql= "SELECT missingTeachers.ID, teachers.short as teShort, missingTeachers.sDay, missingTeachers.sHour, missingTeachers.eDay, missingTeachers.eHour, missingTeachers.sure, missingTeachers.reason FROM missingTeachers INNER JOIN teachers ON teachers.ID = missingTeachers.teacherFK";
 
 //printf("%s,%s,%s",$table, $where, $order);
 if($where!="" && $order!=""){		//Wenn beide Variablen beide gesetzt
@@ -237,7 +236,7 @@ return mysql_query($sqlex);
 
 function selectMissingClass($where,$order){
 
-  $sql= "SELECT missingClasses.ID, classes.name as clName, missingClasses.sDay, missingClasses.sHour, missingClasses.eDay, missingClasses.eHour, missingClasses.sure, missingClasses.reason FROM missingClasses INNER JOIN classes ON classes.ID = missingClasses.class";
+  $sql= "SELECT missingClasses.ID, classes.name as clName, missingClasses.sDay, missingClasses.sHour, missingClasses.eDay, missingClasses.eHour, missingClasses.sure, missingClasses.reason FROM missingClasses INNER JOIN classes ON classes.ID = missingClasses.classFK";
 
 //printf("%s,%s,%s",$table, $where, $order);
 if($where!="" && $order!=""){		//Wenn beide Variablen beide gesetzt
@@ -265,7 +264,7 @@ return mysql_query($sqlex);
 
 }
 
-function selectSubstitude($where,$order){
+function selectSubstitude($where,$order){	//TODO
 
   $sql= "SELECT substitudes.ID, subjects.short as suShort, teachers.short as teShort, substitudes.time, rooms.name as roName, substitudes.hour, substitudes.length, substitudes.hidden, substitudes.sure, substitudes.comment FROM substitudes INNER JOIN subjects ON subjects.ID = substitudes.subject INNER JOIN teachers ON teachers.ID = substitudes.teacher INNER JOIN rooms ON rooms.ID = substitudes.room";
 
@@ -297,7 +296,7 @@ return mysql_query($sqlex);
 
 function selectMonitor($where,$order){
 
-  $sql= "SELECT monitors.ID, monitors.name, monitorMode.name as mode, monitors.file, rooms.name as roName FROM monitors INNER JOIN monitorMode ON monitorMode.ID = monitors.mode INNER JOIN rooms ON rooms.ID = monitors.room";
+  $sql= "SELECT monitors.ID, monitors.name, monitorMode.name as mode, monitors.file, rooms.name as roName FROM monitors INNER JOIN monitorMode ON monitorMode.ID = monitors.mode INNER JOIN rooms ON rooms.ID = monitors.roomFK";
 
 //printf("%s,%s,%s",$table, $where, $order);
 if($where!="" && $order!=""){		//Wenn beide Variablen beide gesetzt
