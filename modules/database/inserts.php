1c2698363b66cc9a4c4441dfa0b5e44fafd40c24
Marco <handle.marco@tele2.at>
2013-08-27 00:08:02 +0200

    Allgemeine FUnktion zum speichern,updaten,loeschen von Datensaetzen und spezifische FUnktion fr clases

--- modules/database/inserts.php
+++ modules/database/inserts.php
@@ -4,7 +4,7 @@
 	 * Autor: Handle Marco
 	 * Version: 0.1.0
 	 * Beschreibung:
-	 *	Select Befehle für die Datenbank
+	 *	Select Befehle fÃ¼r die Datenbank
 	 *
 	 * Changelog:
 	 * 	0.1.0:  27. 07. 2013, Handle Marco - erste Version
@@ -14,39 +14,84 @@
 function classes(){
 
 $post=$_POST;
-
-unset($post["save"]);
-$temp = mysql_fetch_array(mysql_query("SELECT ID FROM sections WHERE short='".$post["seShort"]."'"));
-$post["seShort"] = $temp["ID"];
-$temp = mysql_fetch_array(mysql_query("SELECT ID FROM teachers WHERE short='".$post["teShort"]."'"));
-$post["teShort"] = $temp["ID"];
-$temp = mysql_fetch_array(mysql_query("SELECT ID FROM rooms WHERE name='".$post["roName"]."'"));
-print_r($post["roName"]);
-$post["roName"] = $temp["ID"];
-
 print_r($post);
+unset($post["save"]);
 
 $classesInsert=array("ID" => "","name" => "","sectionFK" => "","teacherFK" => "","roomFK" => "");
 
+$classesInsert["ID"]=$post["ID"];
+$classesInsert["name"]=$post["clName"];
+
+$temp = mysql_fetch_array(mysql_query("SELECT ID FROM sections WHERE short='".$post["seShort"]."'"));
+$classesInsert["sectionFK"] = $temp["ID"];
+$temp = mysql_fetch_array(mysql_query("SELECT ID FROM teachers WHERE short='".$post["teShort"]."'"));
+$classesInsert["teacherFK"] = $temp["ID"];
+$temp = mysql_fetch_array(mysql_query("SELECT ID FROM rooms WHERE name='".$post["roName"]."'"));
+$classesInsert["roomFK"] = $temp["ID"];
+
+if(empty($post["delete"]))
+	saveupdate($classesInsert,"classes");
+else
+	delete($classesInsert["ID"],"classes");
 }
 
 
-function saveUpdate(){
+function saveUpdate($insert,$table){
 
-	if(empty($_POST['ID']))
+	if(empty($insert['ID']))
 	{
 	
+		$col="";
+		$dat="";
+		$len=count($insert);
+		$lauf=1;
+
+		foreach($insert as $i => $p)
+		{
+			if($i != "ID"){
+				$col.= $i;
+				$dat.= "'".$p."'";
+				if($lauf < $len){
+					$col.=" , ";
+					$dat.=" , ";
+				}
+			}
+			$lauf+=1;		
+		}
+
+		$sql="INSERT INTO ".$table." (".$col.") VALUES (".$dat.")";
 	
 	}
-	else if(!empty($_POST['ID']))
+	else if(!empty($insert['ID']))
 	{
+		$dat="";
+		$len=count($insert);
+		$lauf=1;
+		
+		foreach($insert as $i => $p)
+		{
+			if($i != "ID"){
+				$dat.= $i." = '".$p."'";
+
+				if($lauf < $len)
+					$dat.=" , ";
+			}
+			$lauf+=1;		
+		}
 	
-	
+		$sql="UPDATE ".$table." SET ".$dat." WHERE ID = '".$insert["ID"]."'";
 	}
+
+mysql_query($sql);
 
 }
 
+function delete($ID,$table){
 
+$sql="DELETE FROM ".$table." WHERE ID = '".$ID."'";
+mysql_query($sql);
+
+}
 
 
 ?>
