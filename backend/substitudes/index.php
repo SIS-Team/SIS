dd0041f316068fd6c753f54510b14abe8dc5e2d8
Marco <handle.marco@tele2.at>
2013-08-29 22:49:57 +0200

    Aenderungen am Design und der Datenverarbeitung(Wunsch Stecher)

--- backend/substitudes/index.php
+++ backend/substitudes/index.php
@@ -1,33 +1,55 @@
+<script type="text/javascript">
+function Visibility(id) {
+if(document.getElementById('visibleRow'+id).style.visibility=="visible")
+	document.getElementById('visibleRow'+id).style.visibility="collapse";
+else
+	document.getElementById('visibleRow'+id).style.visibility="visible";
+}
+</script>
+
 <?php
 
 	/* /backend/substitude.php
 	 * Autor: Handle Marco
-	 * Version: 0.1.0
+	 * Version: 0.2.0
 	 * Beschreibung:
 	 *	Erstellt die Formulare fuer die Eingabe der Supplierungen
 	 *
 	 * Changelog:
 	 * 	0.1.0:  22. 07. 2013, Handle Marco - erste Version
+	 *  0.2.0:  27. 08. 2013, Handle Marco - Update,Save,delete implementiert
 	 */
 
 include($_SERVER['DOCUMENT_ROOT'] . "/modules/form/form.php");					//Stell die Formularmasken zur Verfügung
 include($_SERVER['DOCUMENT_ROOT'] . "/modules/form/dropdownSelects.php");		//Stellt die Listen für die Dropdownmenüs zur Verfügung
 include($_SERVER['DOCUMENT_ROOT'] . "/modules/general/Main.php");				//Stellt das Design zur Verfügung
 include($_SERVER['DOCUMENT_ROOT'] . "/modules/database/selects.php");			//Stellt die select-Befehle zur Verfügung
+include($_SERVER['DOCUMENT_ROOT'] . "/modules/database/inserts.php");			//Stellt die insert-Befehle zur Verfügung
+
+if($_POST['save']!="")
+	substitudes();
 
 //Formularmaske
-$fields = array(
-	array( "ID", 		"",			 		"hidden", 	"",		"",		"",					""),
-	array( "suShort", 	"Fach: ", 			"dropdown", "8",	"",		$selectSubjects,	""),
-	array( "teShort", 	"Supplierlehrer: ",	"dropdown", "5",	"",		$selectTeachers,	""),
-	array( "time", 		"Datum: ",			"text",		"10",	"",		"",					""),
-	array( "roName",	"Raum: ", 			"dropdown",	"8",	"",		$selectRooms,		""),
-	array( "startHour", "Start-Stunde: ", 	"text",		"5",	"",		"",					""),
-	array( "endHour",	"End-Stunde: ",	 	"text",		"4",	"",		"",					""),
-	array( "hidden",	"Ausblednen? ", 	"checkbox",	"",		"",		"",					""),
-	array( "sure", 		"Sicher? ", 		"checkbox",	"",		"",		"",					""),
-	array( "comment", 	"Kommentar: ", 		"text",		"25",	"",		"",					""),
+$fieldsRow1 = array(
+	array( "ID", 		"",			 		"hidden", 		"",		"",		"",					""),
+	array( "move",	 	"Verschiebung: ", 	"checkboxJava", "8",	"",		"",					""),
+	array( "clName", 	"Klasse: ", 		"dropdown", 	"8",	"",		$selectClasses,		""),
+	array( "suShort", 	"Fach: ", 			"dropdown", 	"8",	"",		$selectSubjects,	""),
+	array( "teShort", 	"Supplierlehrer: ",	"dropdown", 	"5",	"",		$selectTeachers,	""),
+	array( "time", 		"Datum: ",			"text",			"10",	"",		"",					""),
+	array( "roName",	"Raum: ", 			"dropdown",		"8",	"",		$selectRooms,		""),
+	array( "startHour",	"Start-Std.: ", 	"text",			"5",	"",		"",					""),
+	array( "endHour",	"End-Std.: ",	 	"text",			"4",	"",		"",					""),
+	array( "hidden",	"Ausblednen? ", 	"checkbox",		"",		"",		"",					""),
+	array( "sure", 		"Sicher? ", 		"checkbox",		"",		"",		"true",				""),
+	array( "comment", 	"Kommentar: ", 		"text",			"25",	"",		"",					""),
 	);
+	
+$fieldsRow2 = array(
+	array( "newStartHour",  "Neue Start-St.: ", 	"text",		"5",	"",		"",		""),
+	array( "newEndHour",	"Neue End-St.: ",		"text",		"4",	"",		"",		""),
+	);
+
 
 //Seitenheader
 pageHeader("Formular","main");
@@ -35,10 +57,11 @@ pageHeader("Formular","main");
 $result = selectSubstitude("","");			//Rückgabewert des Selects
 
 while ($row = mysql_fetch_array($result)){	//Fügt solange eine neue Formularzeile hinzu, solange ein Inhalt zur Verfügung steht
-	form_new($fields,$row);		//Formular wird erstellt
+print_r($row);
+	form_substitudes($fieldsRow1,$fieldsRow2 ,$row);		//Formular wird erstellt
 }
 
-form_new($fields,false);			//Formular für einen neuen Eintrag
+form_substitudes($fieldsRow1,$fieldsRow2,false);			//Formular für einen neuen Eintrag
 
 //Seitenfooter
 pageFooter();
--- modules/form/form.php
+++ modules/form/form.php
@@ -140,7 +140,131 @@ printf("<table>\n");	//Tabellen Tag auf
   	printf("</tr>\n");
 printf("</table>\n");
 }
-	 
+
+function form_substitudes($fieldRow1,$fieldRow2,$content)
+{	
+
+printf("<table>\n");	//Tabellen Tag auf
+	printf("<form method=\"post\">\n");	//Formular Anfang Tag
+		printf("<tr>\n");	//Zeilen 	Tag auf
+  			//F�r jeden Eintrag im Array field einmal diese Schleife durchlaufen
+			foreach($fieldRow1 as $f) {	
+				
+				if($content!=false && $f[2]!="dropdown")	//Wenn Content mitgeliefert wird und der Typ des aktuellen Inputs kein DropDown, dann Content austauschen
+				{
+					$f[5]=$content[$f[0]];					//Content des aktuellen Inputs austauschen, Name des Array-Indizes ist der name des Inputs
+				}
+				else if($content!=false && $f[2]=="dropdown")	//Wenn Content mitgeliefert wird und der Typ des des aktuellen Inputs ein DropDown ist, dann Sonderbehandlung
+				{
+					foreach($f[5] as $ii => $a)				//F�r jeden Inhalt(Eintr�g der DropDown Liste) wird �berpr�ft ob der content mit dem Einrag in der DropDown Liste �bereinstimmt --> ausgew�hlt
+					{
+						if($a[0]==$content[$f[0]])			//Wenn ein Eintrag mit dem mitgegebenen Eintrag �bereinstimmt, dann in dem Aktuellen Input, beim Inhalt und dem Entsprechenden Eintrag select auf true setzen
+						{	
+							$f[5][$ii][1]=true;
+							break;
+						}
+						else								//sonst leer
+						{
+							$f[5][$ii][1]="";
+						}
+					}
+				}
+	
+				if ($f[2] == "checkbox") {		//Checkbox
+				
+					if($f[5]==true){			//Wenn gecheckt, dann Variable checked auf checked setzen
+						$checked="checked";
+					}
+					else{						//sonst leer
+						$checked="";					
+					}
+					
+					printf("<td>%s <input type=\"checkbox\" name=\"%s\" %s value=\"%s\" %s>\n",		//Checkbox erstellen
+							$f[1], $f[0], $checked, $f[0],$f[6]);
+					
+				}
+				else if ($f[2] == "checkboxJava") {		//Checkbox
+					$collapse="collapse";
+					if($f[5]=="1"){			//Wenn gecheckt, dann Variable checked auf checked setzen
+						$checked="checked";
+						$collapse="visible";				
+					}
+					else{						//sonst leer
+						$checked="";
+						$collapse="collapse";
+					}
+					if(empty($content["ID"]))
+						$ID=0;
+					else
+						$ID=$content["ID"];
+						
+					printf("<td>%s <input type=\"checkbox\" name=\"%s\" %s value=\"%s\" onclick=\"javascript:Visibility(%s)\">\n",		//Checkbox erstellen
+							$f[1], $f[0], $checked, $f[0],$ID);
+					
+				}
+				else if($f[2] == "text") {
+					printf("<td>%s <input type=\"text\" name=\"%s\" size=\"%spx\" value=\"%s\" %s >\n",	//Textbox erstellen
+							$f[1], $f[0], $f[3], $f[5], $f[6]);
+
+				}
+				else if($f[2] == "hidden") {
+					printf("<td><input type=\"hidden\" name=\"%s\" value=\"%s\">\n",			//Versteckten Typ erstellen
+							$f[0], $f[5]);
+
+				}
+				else if($f[2] == "dropdown") {												//Dropdown Men� erstellen
+					$select="";
+					
+					foreach($f[5] as $p)													//F r jeden Men eintrag im Array f einen Eintrag erstellen
+						{
+							if($p[1]==true){													//wenn ausgew hlt selected
+								$select=$p[0];
+							}
+						}
+
+					printf("<td>%s <input value=\"%s\" autocomplete=\"off\" size=\"%s\" list=\"%s\" name=\"%s\"><datalist id=\"%s\"  %s>\n",
+							$f[1], $select, $f[3], $f[0], $f[0], $f[0], $f[6]);
+							
+					foreach($f[5] as $p)													//F�r jeden Men�eintrag im Array f einen Eintrag erstellen
+					{
+							printf("<option value=\"%s\">\n", $p[0]);
+					}
+
+					printf("</datalist></td>\n");
+				}
+				
+			}
+			
+			if($content==false)
+			{
+				form_savedelete(true);
+			}
+			else
+			{
+				form_savedelete(false);
+			}
+				
+  	printf("</tr>\n");
+  	printf("<tr id=\"visibleRow%s\" style=\"visibility:%s\">\n",$ID,$collapse);	//Zeilen 	Tag auf
+  			//F�r jeden Eintrag im Array field einmal diese Schleife durchlaufen
+  			printf("<td colspan=\"7\"></td>\n");
+			foreach($fieldRow2 as $f) {	
+					
+				if($content!=false)	//Wenn Content mitgeliefert, dann Content austauschen
+				{
+					$f[5]=$content[$f[0]];					//Content des aktuellen Inputs austauschen, Name des Array-Indizes ist der name des Inputs
+				}
+
+				printf("<td>%s <input type=\"text\" name=\"%s\" size=\"%spx\" value=\"%s\" %s >\n",	//Textbox erstellen
+						$f[1], $f[0], $f[3], $f[5], $f[6]);	
+								
+			}
+			
+  	printf("</tr>\n");
+	printf("</form>\n");
+printf("</table>\n");
+}
+
 
 /*
  *Erstellt die Schaltfl�chen f�r Speichern und L�schen
@@ -157,66 +281,5 @@ if($new!=true)
 printf("<td><input type=\"submit\" name=\"save\" value=\"Speichern\"></td>\n");	//Submit Button erstellen
 
 }
-
-/*
-function save()
-{
-	$arr = explode(',', $_POST["std"]);			//Stunden aufspalten bei Beistrich und in Array speichern
-		
-		foreach($arr as $stunde){					//für jeden Stundeneintrag wird deis einml abgearbeitet
-			$fls = array();							//leeres Array erstellen für die Namen der Spalten
-			$vals = array();						//leeres Array erstellen für die Spaltenwerte
-			
-			foreach ($fields as $f) {				//für jedes Feld im Formular die Werte und deren Name in die zuvor erstellten Werte schreieben
-				array_push($fls, $f[0]);
-				array_push($vals, $_POST[$f[0]]);
-			}
-			$vals[1] = $stunde;						//Stunde "ausbessern"
-			$fl = implode(",", $fls);				//Beistriche nach jedem Wert einfügen
-			$val = implode("','", $vals);			//Beistriche und Hochkommas einfügen
-			mysql_query("INSERT INTO Supplierungen ($fl) VALUES('$val')");		//In MYSQL Tabelle speichern
-
-
-
-}
-*/
-/*
-
- if (!empty($_POST["save"])) {		//wenn save rückgabe nicht leer
-	if (empty($_POST["ID"])) {		//wenn keine ID --> neuer Eintrag sonst Update
-		$arr = explode(',', $_POST["std"]);			//Stunden aufspalten bei Beistrich und in Array speichern
-		
-		foreach($arr as $stunde){					//für jeden Stundeneintrag wird deis einml abgearbeitet
-			$fls = array();							//leeres Array erstellen für die Namen der Spalten
-			$vals = array();						//leeres Array erstellen für die Spaltenwerte
-			
-			foreach ($fields as $f) {				//für jedes Feld im Formular die Werte und deren Name in die zuvor erstellten Werte schreieben
-				array_push($fls, $f[0]);
-				array_push($vals, $_POST[$f[0]]);
-			}
-			$vals[1] = $stunde;						//Stunde "ausbessern"
-			$fl = implode(",", $fls);				//Beistriche nach jedem Wert einfügen
-			$val = implode("','", $vals);			//Beistriche und Hochkommas einfügen
-			mysql_query("INSERT INTO Supplierungen ($fl) VALUES('$val')");		//In MYSQL Tabelle speichern
-		}
-	}		
-	else {
-		$vals = array();	//leeres Array erstellen für die Werte
-		foreach ($fields as $f) {				//für jedes Feld im Formular die Werte und deren Name in die zuvor erstellten Werte schreieben
-			array_push($vals, $f[0]."='".$_POST[$f[0]]."'");
-		}
-		$val = implode(",", $vals);	//Beistriche nach jedem Wert einfügen
-		mysql_query("UPDATE Supplierungen SET $val WHERE ID=".$_POST["ID"]);		//MYSQL Tabelle updaten
-	}
-}
-else if (!empty($_POST["delete"]) & $_POST["Wirklich"]=="loeschen")	//Wenn löschen und hacken gesetzt EIntrag mit zugehöriger ID löschen
-{
-	if ($_POST["ID"]) {
-		mysql_query("DELETE FROM Supplierungen WHERE ID=".$_POST["ID"]);			
-	}
-
-}
-*/
-	 
-	 
 ?>
+
