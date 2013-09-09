90d9b16fa81912cf7643188872efeac5fa640068
moorgar11 <clara1@aon.at>
2013-09-09 16:01:13 +0200

    Korrekturen

--- substitudes/index.php
+++ substitudes/index.php
@@ -12,8 +12,6 @@
 	 * 	0.1.0:  09. 09. 2013, Weiland Mathias  - erste Version
 	 */
 	 
-include($_SERVER['DOCUMENT_ROOT'] . "/modules/form/form.php");					//Stell die Formularmasken zur Verfügung
-include($_SERVER['DOCUMENT_ROOT'] . "/modules/form/dropdownSelects.php");		//Stellt die Listen für die Dropdownmenüs zur Verfügung
 include($_SERVER['DOCUMENT_ROOT'] . "/modules/general/Main.php");				//Stellt das Design zur Verfügung
 include($_SERVER['DOCUMENT_ROOT'] . "/modules/database/selects.php");			//Stellt die select-Befehle zur Verfügung
 include($_SERVER['DOCUMENT_ROOT'] . "/modules/other/dateFunctions.php");		//Stellt Datumsfunktionen zur Verfügung
@@ -44,7 +42,7 @@ for($count = 0;; $count++)	//Supplierungen ausgeben
   if(empty($substitudes[$count][2]) == true) break;		//Abbruch wenn keine weiteren Einträge
 printf("<tr>");
  printf( "<td>".($count+1)."</td>");	//Tabellenzeilennummer
- printf( "<td>".$substitudes[$count][12]."-".$substitudes[$count][13]."</td>");// Anfangsstunde - Endstunde
+ printf( "<td>".$substitudes[$count][7]."</td>");// supplierte Stunde 
  printf( "<td>".$substitudes[$count][2]."</td>");	//Klassenname
  printf( "<td>".$substitudes[$count][4]."</td>");	//supplierender Lehrer
  printf( "<td>".$substitudes[$count][3]."</td>");	//Fach
@@ -58,7 +56,7 @@ function getSubstitude($date){	//Supplierungen des gewählten Datums abrufen
 	global $substitudes;
 			$where = "time = '".$date."'";
 				
-			$substitude_sql = selectSubstitude($where,"")	
+			$substitude_sql = selectSubstitude($where,"startHour")	
 			or die("MySQL-Error: ".mysql_error());
 			while($substitude = mysql_fetch_array($substitude_sql)) {    
 		 	$substitudes[]=$substitude;}	
