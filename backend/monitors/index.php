01ec576a29069c7f4c834a037a74e3f7f65028bc
overflowerror <mail@overflowerror.com>
2013-09-17 15:50:14 +0200

    frontend für monitor-backend

--- backend/monitors/index.php
+++ backend/monitors/index.php
@@ -1,54 +1,81 @@
 <?php
 
-	/* /backend/monitors.php
-	 * Autor: Handle Marco
-	 * Version: 0.2.0
+	/* /backend/monitors/index.php
+	 * Autor: Buchberger Florian
+	 * Version: 0.1.0
 	 * Beschreibung:
-	 *	Erstellt die Formulare fuer die Eingabe der Monitore
+	 *	Erstellt die Formulare für die Eingabe der Monitore
 	 *
 	 * Changelog:
-	 * 	0.1.0:  22. 07. 2013, Handle Marco - erste Version
-	 *  0.2.0:	27. 07. 2013, Handle Marco - selects hinzugefügt
+	 * 	0.1.0:  09. 09 2013, Buchberger Florian - neue Version
 	 */
 
+	include_once($_SERVER['DOCUMENT_ROOT'] . "/modules/general/Main.php");
+	include_once($_SERVER['DOCUMENT_ROOT'] . "/modules/monitors/Main.php");
+	pageHeader("Monitore", "main");
 
-include($_SERVER['DOCUMENT_ROOT'] . "/modules/formular/formular.php");
-include($_SERVER['DOCUMENT_ROOT'] . "/modules/general/Connect.php");
-include($_SERVER['DOCUMENT_ROOT'] . "/modules/general/Main.php");
-include($_SERVER['DOCUMENT_ROOT'] . "/modules/database/selects.php");
+	$monitors = getAllMonitors();
+	$rooms = mysql_query("SELECT `name` FROM `rooms`");
+	$modes = mysql_query("SELECT `name` FROM `monitorMode`");
+?>
+<form action="?" method="post" enctype="multipart/form-data">
+	<datalist id="rooms">
+<?php
+	while ($room = mysql_fetch_object($rooms)) {
+		echo "		<option value=\"" . $room->name . "\"></option>";
+	}
+?>
+	</datalist>
+	<datalist id="rooms">
+<?php
+	while ($modes = mysql_fetch_object($modes)) {
+		echo "		<option value=\"" . $modes->name . "\"></option>";
+	}
+?>
+	</datalist>
+	<table>
+		<tr>
+			<th></th>
+			<th>Name</th>
+			<th>Raum</th>
+			<th>Modus</th>
+			<th>Datei</th>
+		</tr>
+<?php
+	while ($monitor = mysql_fetch_object($monitors)) {
+		echo "	<tr>";
+		echo "		<td><input name=\"m" . $monitor->id . "\" type=\"checkbox\"></td>";
+		echo "		<td>" . $monitor->name . "</td>";
+		echo "		<td>" . $monitor->room . "</td>";
+		echo "		<td>" . $monitor->type . "</td>";
+		echo "		<td>" . $monitor->file . "</td>";
+		echo "	</tr>";
+	}
+?>
+	</table>
+	<br />
+	<table>
+		<tr>
+			<td>Raum: (leer f&uuml;r unver&auml;ndert) </td>
+			<td><input type="text" autocomplete="off" list="rooms" name="room" style="width: 100%"></td>
+		</tr>
+		<tr>
+			<td>Modus: (leer f&uuml;r unver&auml;ndert) </td>
+			<td><input type="text" autocomplete="off" list="modes" name="modes" style="width: 100%"></td>
+		</tr>
+		<tr>
+			<td>Datei: (leer f&uuml;r unver&auml;ndert) </td>
+			<td><input type="file" name="file" maxlength="800000000" accept="image/jpeg image/gif image/png video/mpeg" style="width: 100%"></td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<input type="submit" value="Absenden" style="width: 100%">
+			</td>
+		</tr>
+	</table>
+</form>
+<?php
 
-$selectRooms = array(
-	array("I164", 	""),
-	array("Test2", 	""),
-	array("Test3", 	""),
-	);
+	pageFooter();
 
-$selectMonitorMode = array(
-	array("bild", 	""),
-	array("Test2", 	""),
-	array("Test3", 	""),
-	);
-
-
-$fields = array(
-	array( "ID", 	"",			"hidden", 	"",		"",		"",					""),
-	array( "name", 	"Name: ", 	"text", 	"15",	"",		"",					""),
-	array( "mode", 	"Modus: ", 	"dropdown", "8",	"",		$selectMonitorMode,	""),
-	array( "file", 	"File: ", 	"text",		"20",	"",		"",					""),
-	array( "roName","Raum: ", 	"dropdown",	"5",	"",		$selectRooms,		""),	
-	);
-
-
-pageHeader("Formular","main");
-
-$result = selectMonitor("","");
-while ($row = mysql_fetch_array($result)){
-	print_r($row);
-	form_new($fields,$row);
-}
-
-
-form_new($fields,false);
-
-pageFooter();
 ?>
