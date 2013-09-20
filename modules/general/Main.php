e8ef26d82329b53a0343b80b00258442a409ce53
overflowerror <mail@overflowerror.com>
2013-09-20 18:38:46 +0200

    maskierungs-befehl + message klassen (css)

--- modules/general/Main.php
+++ modules/general/Main.php
@@ -6,6 +6,7 @@
 	 *	Bindet alle grundlegenden Module ein.
 	 *
 	 * Changelog:
+	 *	0.1.2:	20. 09.	2013, Buchberger Florian - ascii-Zeichen-Filter-Funktion
 	 *	0.1.1:	19. 09. 2013, Buchberger Florian - Cookie check
 	 * 	0.1.0:  22. 06. 2013, Buchberger Florian - erste Version
 	 */
@@ -15,5 +16,10 @@
 	include_once($_SERVER['DOCUMENT_ROOT'] . "/modules/general/SessionManager.php");
 	include_once($_SERVER['DOCUMENT_ROOT'] . "/modules/general/Connect.php");
 	include_once($_SERVER['DOCUMENT_ROOT'] . "/modules/general/Site.php");
+
+	// maskiert alle nicht-ascii-Zeichen im Parameter 
+	function sanitize($s) {
+       		return preg_replace('/[^a-zA-Z0-9_.]/', '_', $s);
+	}
 
 ?>
