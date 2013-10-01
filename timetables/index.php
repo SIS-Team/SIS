4dde86835415103f0d3fb062f5d7a55bbc6addd1
moorgar11 <clara1@aon.at>
2013-10-01 20:09:20 +0200

    Stundenplan fuer Monitore v2

--- timetables/index.php
+++ timetables/index.php
@@ -10,89 +10,100 @@
 	 *
 	 * Changelog:
 	 * 	0.1.0:  24. 09. 2013, Weiland Mathias  - erste Version
+	 *  0.2.0:  01. 10. 2013, Weiland Mathias  - zweite Version
 	 */
 	 
 include_once($_SERVER['DOCUMENT_ROOT'] . "/modules/general/Main.php");				//Stellt das Design zur Verfügung
 include_once($_SERVER['DOCUMENT_ROOT'] . "/modules/database/selects.php");			//Stellt die select-Befehle zur Verfügung
 
-
-
+$roName =$_GET['name'];
+if(!isset($roName))die("Kein Raumname vorhanden");
 pageHeader("Formular","main");
 
 //Tabellenkopfausgabe
 		echo "<table border = 1>";
 		echo "<tr>";
 		echo "<th>Stunde</th>";
-		echo "<th></th>";
-		echo "<th>Mo</th>";
-		echo "<th>Di</th>";
-		echo "<th>Mi</th>";
-		echo "<th>Do</th>";
-		echo "<th>Fr</th>";
+		echo "<th colspan = 2>Mo</th>";
+		echo "<th colspan = 2>Di</th>";
+		echo "<th colspan = 2>Mi</th>";
+		echo "<th colspan = 2>Do</th>";
+		echo "<th colspan = 2>Fr</th>";
 		echo "</tr>";
 		
-/*		
-		
-		for($count = 1;$count <= 16; $count++)	//Supplierungen ausgeben
-		{
-		 
-		getLessons("I310",$count);
-		 echo "<tr>";
-		 echo "<td>".$count."</td>";
-		 echo "<td>08:00 - 08:50</td>";
-		 echo "<td>".$lessons[0][5]."</td>";	//Fach Montag
-		 echo "<td>".$lessons[1][5]."</td>";	//Fach Dienstag
-	 	 echo "<td>".$lessons[2][5]."</td>";	//Fach Mittwoch
-		 echo "<td>".$lessons[3][5]."</td>";	//Fach Donnerstag
-		 echo "<td>".$lessons[4][5]."</td>";	//Fach Freitag
-		 echo "</tr>";
-		$lessons = array();
-		}
-		echo "</table>";    */
 
-$array = ngetLessons("I310");
-$hours = array();
-for ($i = 0; $i < count($array); $i++) {
+
+$array = ngetLessons($roName);  //fragt Studen ab, die in der gesuchten Klasse stattfinden
+$hours = array();			   //ertellt ein leeres Array
+for ($i = 0; $i < count($array); $i++) {	
  	$index = $array[$i]->startHour;
 	if (!$hours[$index])
 		$hours[$index] = array();
 	$hours[$index][$array[$i]->weekdayShort] = $array[$i];
 }
 
-	print_r($hours);
+//	print_r($array);
+//print_r($hours[5]["Fr"]);
+//echo "<br><br>Hihi: ".$hours[1]["Mo"]->suShort."<br><br>";
 
 for ($i = 1; $i < 17; $i++) {
  	echo "<tr>";
- 	if (!$hours[$i]){ echo "check";
+ 	echo "<td>".$i."</td>";			//gibt Stundennummer an
+ 	if (!$hours[$i]){// echo "check";
 		for ($j = 0; $j < 5; $j++) 
-			echo "<td></td>";
-	} else { echo "checkv2";
+			echo "<td>&#160;</td><td>&#160;</td>"; //Ausgabe eines leeren Feldes, wenn keine Stunde vorhanden
+	} else {// echo "checkv2";
 		for ($j = 0; $j < 5; $j++) {
 			switch($j) {
-			case 0:
-				echo " (CASE0)". $i;
-				if ($array[$i]['Mo'])
-					echo "<td>" . $array[$i]['Mo']->teShort . "</td>";
-				echo " (ENDE VON CASE0)";
+			case 0:	//Montag
+					echo "<td>" . $hours[$i]['Mo']->suShort ."&#160;</td><td>" . $hours[$i]['Mo']->clName.	"&#160;  </td>";	//gibt 	Fach und Stunde aus
+					
+					if(($hours[$i]['Mo']->endHour) > $i) //kopiert aktuelle Stunde in nächste Stunde, wenn mehr als eine Stunde nacheinander stattfindet
+					{$hours[$i+1]['Mo']->suShort = $hours[$i]['Mo']->suShort;
+					 $hours[$i+1]['Mo']->clName = $hours[$i]['Mo']->clName;
+					 $hours[$i+1]['Mo']->endHour = $hours[$i]['Mo']->endHour;	
+					}
 				break;
-			case 1:
-				if ($array[$i]['Di'])
-					echo "<td>" . $array[$i]['Di']->suShort . "</td>";
+				
+			case 1:	//Dienstag
+					echo "<td>" . $hours[$i]['Di']->suShort ."&#160;</td><td>". $hours[$i]['Di']->clName . "&#160;  </td>";
+					
+					if(($hours[$i]['Di']->endHour) > $i) 
+					{$hours[$i+1]['Di']->suShort = $hours[$i]['Di']->suShort;
+					 $hours[$i+1]['Di']->clName = $hours[$i]['Di']->clName;
+					 $hours[$i+1]['Di']->endHour = $hours[$i]['Di']->endHour;	
+					}
 				break;
-			case 2:
-				if ($array[$i]['Mi'])
-						echo "<td>" . $array[$i]['Mi']->suShort . "</td>";
+				
+			case 2: //Mittwoch
+					echo "<td>" . $hours[$i]['Mi']->suShort ."&#160;</td><td>". $hours[$i]['Mi']->clName . "&#160;  </td>";
+					if(($hours[$i]['Mi']->endHour) > $i)
+					{$hours[$i+1]['Mi']->suShort = $hours[$i]['Mi']->suShort;
+					 $hours[$i+1]['Mi']->clName = $hours[$i]['Mi']->clName;
+					 $hours[$i+1]['Mi']->endHour = $hours[$i]['Mi']->endHour;	
+					}
 				break;
-			case 3:
-				if ($array[$i]['Do'])
-					echo "<td>" . $array[$i]['Do']->suShort . "</td>";
+				
+			case 3: //Donnerstag
+					echo "<td>" . $hours[$i]['Do']->suShort ."&#160;</td><td>". $hours[$i]['Do']->clName . "&#160;  </td>";
+					if(($hours[$i]['Do']->endHour) > $i) 
+					{$hours[$i+1]['Do']->suShort = $hours[$i]['Do']->suShort;
+					 $hours[$i+1]['Do']->clName = $hours[$i]['Do']->clName;
+					 $hours[$i+1]['Do']->endHour = $hours[$i]['Do']->endHour;	
+					}
 				break;
-			case 4:
-				if ($array[$i]['Fr'])
-					echo "<td>" . $array[$i]['Fr']->suShort . "</td>";
+				
+			case 4: //Freitag
+					echo "<td>" . $hours[$i]['Fr']->suShort ."&#160;</td><td>". $hours[$i]['Fr']->clName . "&#160;  </td>";
+					if(($hours[$i]['Fr']->endHour) > $i)
+					{$hours[$i+1]['Fr']->suShort = $hours[$i]['Fr']->suShort;
+					 $hours[$i+1]['Fr']->clName = $hours[$i]['Fr']->clName;
+					 $hours[$i+1]['Fr']->endHour = $hours[$i]['Fr']->endHour;	
+					}
 				break;
-			default:
-				echo "blublub";
+				
+			default: //Fehler
+				echo "Error";
 				break;
 			}
 		}
@@ -101,7 +112,6 @@ for ($i = 1; $i < 17; $i++) {
 }		
 		echo "</table>";
 
-	echo "All over your faces!";
 
 function ngetLessons($roomName) {		//Abfrgae von Stunden im vorgegebenen Raum
 	if ($roomName) 
@@ -114,21 +124,7 @@ function ngetLessons($roomName) {		//Abfrgae von Stunden im vorgegebenen Raum
 	}
 	return $array;
 }
-/*
-function getLessons($roomName,$hour)
-{
-	global $lessons; 
-			$where = ""; 
- 			if(!empty($roomName)) $where = "rooms.Name = '".$roomName . "'"; 
-			if(!empty($roomName) && !empty($hour)) $where .= " AND "; 
-			if(!empty($hour)) $where .= " hoursStart.hour <= '" . $hour . "' AND '" . $hour . "' <=hoursEnd.hour "; 
-			$lessons_sql = selectLesson($where,"")	
-			or die("MySQL-Error: ".mysql_error());
-			while($lesson = mysql_fetch_array($lessons_sql)) {    
-		 		$lessons[]=$lesson;
-			}
-			print_r($lessons[0]);	//Kontrolle des Ergebnisses 
-}*/
+
 
 
 pageFooter();
