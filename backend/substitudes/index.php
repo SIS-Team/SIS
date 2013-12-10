c21f86ed2a87f5212e8c9055919bef5f51791f04
Marco <handle.marco@tele2.at>
2013-12-10 17:36:53 +0100

    KLeine Aenderungen und Beginn Funktion fuer das Speichern von Supplierungen

--- backend/substitudes/index.php
+++ backend/substitudes/index.php
@@ -1,20 +1,3 @@
-<script type="text/javascript">
-
-//Funktion zum ein und ausblenden der neuen Stunden
-//id = Zeilennummer
-function Visibility(id) {
-
-	if(document.getElementById('visibleRow'+id).style.visibility=="visible")	//Wenn die Zeile sichtbar
-		document.getElementById('visibleRow'+id).style.visibility="collapse";	//Zeile unsichtbar
-	else																		//sonst
-		document.getElementById('visibleRow'+id).style.visibility="visible";	//sichtbar machen
-}
-
-function failAlert(){ 
-	alert("Es konnte keine Stunde für\ndiese Supplierung gefunden werden.\nBitte tragen sie diesen Lehrer\nals fehlend ein, oder\nkorrigieren sie die Eingabe.");
-}
-</script>
-
 <?php
 
 	/* /backend/substitude.php
@@ -77,12 +60,30 @@ generateAdminMenu();
 
 
 //Seitenheader
-pageHeader("Formular","main");
+//pageHeader("Formular","main");
+?>
+<script type="text/javascript">
+
+//Funktion zum ein und ausblenden der neuen Stunden
+//id = Zeilennummer
+function Visibility(id) {
+
+	if(document.getElementById('visibleRow'+id).style.visibility=="visible")	//Wenn die Zeile sichtbar
+		document.getElementById('visibleRow'+id).style.visibility="collapse";	//Zeile unsichtbar
+	else																		//sonst
+		document.getElementById('visibleRow'+id).style.visibility="visible";	//sichtbar machen
+}
+
+function failAlert(){ 
+	alert("Es konnte keine Stunde für\ndiese Supplierung gefunden werden.\nBitte tragen sie diesen Lehrer\nals fehlend ein, oder\nkorrigieren sie die Eingabe.");
+}
+</script>
+<?php
 
 $date = dateChange($date);		//Datumsauswahl erzeugen
 $fieldsRow1[5][5] = $date;	//Standartdatum ins Formular schreiben
 
-$where = "substitudes.time = '".$date."'";		//Filter
+$where = "substitudes.time = '".$date."' AND substitudes.display= 1";		//Filter
 $sort = "classes.name, hoursStart.hour";		//Sortierung nach dem Klassenname und der Startstunde
 
 $result = selectSubstitude($where,$sort);			//Rückgabewert des Selects
@@ -94,5 +95,5 @@ while ($row = mysql_fetch_array($result)){	//Fügt solange eine neue Formularzeil
 form_substitudes($fieldsRow1,$fieldsRow2,false);			//Formular für einen neuen Eintrag
 
 //Seitenfooter
-pageFooter();
+//pageFooter();
 ?>
