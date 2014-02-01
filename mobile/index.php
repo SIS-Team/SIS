8f90f0ba4db21881167945daa8484f1b197564e6
matrix79 <p.machac@icloud.com>
2014-02-01 17:04:46 +0100

    mobile site and new background
    added *.woff font for mobile devices
    changed background to jpg with lower size

--- mobile/index.php
+++ mobile/index.php
@@ -2,12 +2,13 @@
 <?php
 	/* /mobile/index.php
 	 * Autor: Machac Philipp
-	 * Version: 1.0.0
+	 * Version: 1.0.1
 	 * Beschreibung:
 	 *	Auswahlseite fuer mobile Geraete.
 	 *
 	 * Changelog:
 	 * 	1.0.0:  31.01.2014, Machac Philipp - Erste Seite
+	 *	1.0.1:	01.02.2014, Machac Philipp - Pfade angepasst
 	 */
 	include("../config.php");
 ?>
@@ -16,23 +17,23 @@
    		 <title>SIS.Mobile</title>
          
          <!-- Favicon -->
-		 <link rel="shortcut icon" href="favicon.ico" type="image/x-ico" />
-		 <link rel="icon" href="favicon.ico" type="image/x-ico" />
+		 <link rel="shortcut icon" href="../favicon.ico" type="image/x-ico" />
+		 <link rel="icon" href="../favicon.ico" type="image/x-ico" />
          <!-- Includes -->
-		 <link rel="stylesheet" type="text/css" href="mobile.css" />
+		 <link rel="stylesheet" type="text/css" href="../data/styles/mobile.css" />
 	</head>
     <body>    
         <div id="content">
           <h1>Mobile Apps</h1>
                 <center>
                     <a href="Placeholder TODO" title="App f&uuml;r Windows Phone ansehen">
-                        <img src="<?php echo RELATIVE_ROOT; ?>/images/windowsphone.svg" alt="Windows Phone Store">
+                        <img src="<?php echo RELATIVE_ROOT; ?>/data/images/mobile/windowsphone.svg" alt="Windows Phone Store">
                     </a>
                     <a href="https://itunes.apple.com/at/app/htlinn/id423243235?mt=8" title="App f&uuml;r iOS ansehen">
-                        <img src="<?php echo RELATIVE_ROOT; ?>/images/ios.svg" alt="App Store">
+                        <img src="<?php echo RELATIVE_ROOT; ?>/data/images/mobile/ios.svg" alt="App Store">
                     </a>
                     <a href="Placeholder TODO" title="App f&uuml;r Android ansehen">
-                        <img src="<?php echo RELATIVE_ROOT; ?>/images/android.svg" alt="GooglePlay Store">
+                        <img src="<?php echo RELATIVE_ROOT; ?>/data/images/mobile/android.svg" alt="GooglePlay Store">
                     </a>
                 </center>
                     <br />
@@ -41,7 +42,7 @@
           <h1>Desktop Version</h1>
                 <center>
                     <a href="<?php echo RELATIVE_ROOT; ?>/?noJS&noMobile" title="Desktop Version der Seite besuchen">
-                        <img src="<?php echo RELATIVE_ROOT; ?>/images/desktop.svg" alt="Desktop Version">
+                        <img src="<?php echo RELATIVE_ROOT; ?>/data/images/mobile/desktop.svg" alt="Desktop Version">
                     </a>
                     <br />
                     <br /> 
