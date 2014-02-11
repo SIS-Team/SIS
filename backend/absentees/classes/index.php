2604c7bbf1b0cd18a321da38865b1e0c9a02c8c2
Marco Handle <handle.marco@tele2.at>
2014-02-11 16:53:48 +0100

    Fehlerkorrektur und Premission

--- backend/absentees/classes/index.php
+++ backend/absentees/classes/index.php
@@ -11,16 +11,17 @@
 	 *  0.2.0:  27. 08. 2013, Handle Marco - Update,Save,delete implementiert
 	 */
 	 
-	include("../../../config.php");
+include("../../../config.php");
 include_once(ROOT_LOCATION . "/modules/general/Main.php");				//Stellt das Design zur Verfügung
 include_once(ROOT_LOCATION . "/modules/form/form.php");					//Stell die Formularmasken zur Verfügung
-include_once(ROOT_LOCATION . "/modules/form/dropdownSelects.php");		//Stellt die Listen für die Dropdownmenüs zur Verfügung
 include_once(ROOT_LOCATION . "/modules/database/selects.php");			//Stellt die select-Befehle zur Verfügung
 include_once(ROOT_LOCATION . "/modules/database/inserts.php");					//Stell die Formularmasken zur Verfügung
 include_once(ROOT_LOCATION . "/modules/other/dateChange.php");					//Stell die Formularmasken zur Verfügung
 
+if (!($_SESSION['rights']['root'] || $_SESSION['rights']['N'] || $_SESSION['rights']['W'] || $_SESSION['rights']['E'] || $_SESSION['rights']['M']))
+	exit();
 
-if($_POST['save']!="")
+if(!empty($_POST['save']) && $_POST['save']!="")
 	missingClasses();
 
 if (empty($_POST["date"])) {		//wenn nichts zurückgegeben wird, dann heute
@@ -31,6 +32,10 @@ else {								//sonst zurückgegebenes Datum
 }
 
 
+//Seitenheader
+pageHeader("Formular","main");
+
+include_once(ROOT_LOCATION . "/modules/form/dropdownSelects.php");		//Stellt die Listen für die Dropdownmenüs zur Verfügung
 //Formularmaske
 $fields = array(
 	array( "ID", 		"",			 		"hidden", 	"",		"",		"",					""),
@@ -43,12 +48,6 @@ $fields = array(
 	array( "reason", 	"Grund: ", 			"text",		"30",	"",		"",					""),
 	);
 
-include(ROOT_LOCATION . "/modules/general/Menu.php");
-generateAdminMenu();
-
-
-//Seitenheader
-pageHeader("Formular","main");
 
 $date = dateChange($date);
 $fields["2"]["5"] = $date;
