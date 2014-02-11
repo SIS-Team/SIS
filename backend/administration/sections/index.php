30bcefb467a0e71c3b5bab35f02c58ba23d407cb
Marco Handle <handle.marco@tele2.at>
2014-02-11 14:31:33 +0100

    Fehlerkrrektur und Premission hinzugefuegt

--- backend/administration/sections/index.php
+++ backend/administration/sections/index.php
@@ -14,21 +14,17 @@
 	include("../../../config.php");
 
 include_once(ROOT_LOCATION . "/modules/form/form.php");					//Stell die Formularmasken zur Verfügung
-include_once(ROOT_LOCATION . "/modules/form/dropdownSelects.php");		//Stellt die Listen für die Dropdownmenüs zur Verfügung
 include_once(ROOT_LOCATION . "/modules/general/Main.php");				//Stellt das Design zur Verfügung
 include_once(ROOT_LOCATION . "/modules/database/selects.php");			//Stellt die select-Befehle zur Verfügung
 include_once(ROOT_LOCATION . "/modules/database/inserts.php");			//Stellt die insert-Befehle zur Verfügung
 
-if($_POST['save']!="")
+if (!($_SESSION['rights']['root']))
+	exit();
+
+
+if(!empty($_POST['save']) && $_POST['save']!="")
 	sections();
 
-//Formularmaske
-$fields = array(
-	array( "ID", 		"",			 			"hidden", 	"",		"",		"",					""),
-	array( "seName",	"Name: ", 				"text", 	"40",	"",		"",					""),
-	array( "seShort",	"Kürzel: ",			 	"text",	 	"5",	"",		"",					""),
-	array( "teShort",	"Abteilungsleiter: ", 	"dropdown", "5",	"",		$selectTeachers,	""),
-	);
 
 include(ROOT_LOCATION . "/modules/general/Menu.php");
 generateAdminMenu();
@@ -36,6 +32,16 @@ generateAdminMenu();
 	
 //Seitenheader
 pageHeader("Formular","main");
+
+include_once(ROOT_LOCATION . "/modules/form/dropdownSelects.php");		//Stellt die Listen für die Dropdownmenüs zur Verfügung
+//Formularmaske
+$fields = array(
+	array( "ID", 		"",			 			"hidden", 	"",		"",		"",					""),
+	array( "seName",	"Name: ", 				"text", 	"40",	"",		"",					""),
+	array( "seShort",	"K&uuml;rzel: ",	 	"text",	 	"5",	"",		"",					""),
+	array( "teShort",	"Abteilungsleiter: ", 	"dropdown", "5",	"",		$selectTeachers,	""),
+	);
+
 
 $result = selectSection("","");				//Rückgabewert des Selects
 
