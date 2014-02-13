5e3609444224bbac57acf949aac0ec5302229898
overflowerror <mail@overflowerror.com>
2014-02-13 23:06:26 +0100

    git says there are changes

--- modules/general/VirtualSessionManager.php
+++ modules/general/VirtualSessionManager.php
@@ -13,6 +13,7 @@
 			$this->name = $name;
 		}
 		public function init() {
+			global $_SESSION;
 			if (!isset($_SESSION['vSession']))
 				$_SESSION['vSession'] = array();
 			if (!isset($_SESSION['vSession'][$this->name])) {
@@ -24,15 +25,23 @@
 			}
 		}
 		public function exists() {
+			global $_SESSION;
 			return isset($_SESSION['vSession'][$this->name]);
 		}
+		public function destroy() {
+			global $_SESSION;
+			unset($_SESSION['vSession'][$this->name]);
+		}
 		public function setAttribute($name, $value) {
+			global $_SESSION;
 			$_SESSION['vSession'][$this->name][$name] = $value;
 		}
 		public function getAttribute($name) {
+			global $_SESSION;
 			return $_SESSION['vSession'][$this->name][$name];
 		}
 		public function attributeIsset($name) {
+			global $_SESSION;
 			return isset($_SESSION['vSession'][$this->name][$name]);
 		}
 	}
