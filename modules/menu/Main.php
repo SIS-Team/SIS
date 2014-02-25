9bbef0972b5f3a69445608e40112281f04aa17ea
overflowerror <mail@overflowerror.com>
2014-02-25 11:36:47 +0100

    doctype xhtml 1.0 strict

--- modules/menu/Main.php
+++ modules/menu/Main.php
@@ -21,7 +21,7 @@
 	function generateMenu() {
 		global $back, $headerText, $buttons, $name;
 ?>
-<!DOCTYPE xhtml>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
 		<script>
