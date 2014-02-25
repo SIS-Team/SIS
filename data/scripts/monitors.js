a3bb5881a32477ce88efa4575c27771931ddfece
overflowerror <mail@overflowerror.com>
2014-02-25 18:50:21 +0100

    print image

--- data/scripts/monitors.js
+++ data/scripts/monitors.js
@@ -217,3 +217,64 @@ var getMedia = function (response) {
 	}
 	return text;
 }
+
+
+
+
+
+
+var scope = window;
+
+function currentYPosition() {
+	if (scope.pageYOffset) 
+		return scope.pageYOffset;
+	if (scope.document.documentElement && scope.document.documentElement.scrollTop)
+		return scope.document.documentElement.scrollTop;
+	if (scope.document.body.scrollTop) 
+		return scope.document.body.scrollTop;
+	return 0;
+} 
+
+function elmYPosition(anchorName) {
+	var elm = scope.document.getElementById(anchorName);
+	var y = elm.offsetTop;
+	var node = elm;
+	while (node.offsetParent && node.offsetParent != scope.document.body) {
+		node = node.offsetParent;
+		y += node.offsetTop;
+	}
+	return y;
+}
+
+function smoothScroll(anchorName) {
+	var startY = currentYPosition();
+	var stopY = elmYPosition(anchorName);
+	var distance = (stopY > startY) ? (stopY - startY) : (startY - stopY);
+	if (distance < 100) {
+		scrollTo(0, stopY);
+		return;
+	}
+	var speed = Math.round(distance / 100);
+	if (speed >= 20) 
+		speed = 20;
+	var step = 3);
+	var leapY = (stopY > startY) ? (startY + step) : (startY - step);
+	var timer = 0;
+	if (stopY > startY) {
+		for (var i = startY; i < stopY; i += step) {
+			setTimeout("scope.scrollTo(0, " + leapY + ")", timer * speed);
+			leapY += step; 
+			if (leapY > stopY) 
+				leapY = stopY; 
+			timer++;
+		} 
+		return;
+	}	
+	for (var i = startY; i > stopY; i -= step) {
+		setTimeout("scope.scrollTo(0, " + leapY + ")", timer * speed);
+		leapY -= step; 
+		if (leapY < stopY) 
+			leapY = stopY; 
+		timer++;
+	}
+}
