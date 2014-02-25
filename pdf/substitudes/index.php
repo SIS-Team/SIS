59fced26067aed1a32742d92a6ab96174de5ce3b
moorgar11 <clara1@aon.at>
2014-02-25 19:12:12 +0100

    please change the link to this @marco1995

--- pdf/substitudes/index.php
+++ pdf/substitudes/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../config.php");	 
+include_once("../../config.php");	 
 require_once(ROOT_LOCATION . "/modules/external/fpdf/fpdf.php");
 include_once(ROOT_LOCATION . "/modules/general/Connect.php");			
 include_once(ROOT_LOCATION . "/modules/general/SessionManager.php");
