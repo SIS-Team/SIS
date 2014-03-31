e4666a860404014b370e4e162c0f5a52451e1eec
Marco Handle <handle.marco@tele2.at>
2014-03-31 17:12:26 +0200

    Zusammenfassen der OS

--- modules/other/getBrowser.php
+++ modules/other/getBrowser.php
@@ -53,61 +53,19 @@ function getBrowser($user_agent) {
 
     if (preg_match('/windows|win32/i', $visitor_user_agent)) {
         $os_platform    =   'Windows';
-        if (preg_match('/windows nt 6.3/i', $visitor_user_agent)) {
-            $os_platform    .=  " 8.1";
-        }else if (preg_match('/windows nt 6.2/i', $visitor_user_agent)) {
-            $os_platform    .=  " 8";
-        } else if (preg_match('/windows nt 6.1/i', $visitor_user_agent)) {
-            $os_platform    .=  " 7";
-        } else if (preg_match('/windows nt 6.0/i', $visitor_user_agent)) {
-            $os_platform    .=  " Vista";
-        } else if (preg_match('/windows nt 5.2/i', $visitor_user_agent)) {
-            $os_platform    .=  " Server 2003/XP x64";
-        } else if (preg_match('/windows nt 5.1/i', $visitor_user_agent) || preg_match('/windows xp/i', $visitor_user_agent)) {
-            $os_platform    .=  " XP";
-        } else if (preg_match('/windows nt 5.0/i', $visitor_user_agent)) {
-            $os_platform    .=  " 2000";
-        } else if (preg_match('/windows me/i', $visitor_user_agent)) {
-            $os_platform    .=  " ME";
-        } else if (preg_match('/win98/i', $visitor_user_agent)) {
-            $os_platform    .=  " 98";
-        } else if (preg_match('/win95/i', $visitor_user_agent)) {
-            $os_platform    .=  " 95";
-        } else if (preg_match('/win16/i', $visitor_user_agent)) {
-            $os_platform    .=  " 3.11";
-    	} else if (preg_match('/phone/i', $visitor_user_agent)) {
+        if (preg_match('/phone/i', $visitor_user_agent)) {
         	$os_platform    .=  " Phone";
-        	if (preg_match('/phone 7/i', $visitor_user_agent)) {
-        		$os_platform    .=  " 7";
-      		} else if (preg_match('/phone 8/i', $visitor_user_agent)) {
-       			$os_platform    .=  " 8";
-			}
         }
     } else if (preg_match('/macintosh|mac os x/i', $visitor_user_agent)) {
-        $os_platform    =   'Mac';
-        if (preg_match('/macintosh/i', $visitor_user_agent)) {
-            $os_platform    .=  " OS X";
-        } else if (preg_match('/mac_powerpc/i', $visitor_user_agent)) {
-            $os_platform    .=  " OS 9";
-        }
+        $os_platform    =   'Mac OS X';
     } else if (preg_match('/linux/i', $visitor_user_agent)) {
         $os_platform    =   "Linux";
     }
 
     if (preg_match('/iphone/i', $visitor_user_agent)) {
         $os_platform    =   "iPhone";
-        if (preg_match('/OS 7/i', $visitor_user_agent)) {
-            $os_platform    .=  " 7.x.x";
-        } else if (preg_match('/OS 6/i', $visitor_user_agent)) {
-            $os_platform    .=  " 6.x.x";
-		}
     } else if (preg_match('/android/i', $visitor_user_agent)) {
         $os_platform    =   "Android";
-        if (preg_match('/android 2/i', $visitor_user_agent)) {
-            $os_platform    .=  " 2.x.x";
-        } else if (preg_match('/android 4/i', $visitor_user_agent)) {
-            $os_platform    .=  " 4.x.x";
-		}
     } else if (preg_match('/blackberry/i', $visitor_user_agent)) {
         $os_platform    =   "BlackBerry";
     } else if (preg_match('/webos/i', $visitor_user_agent)) {
@@ -116,20 +74,8 @@ function getBrowser($user_agent) {
         $os_platform    =   "Symbian";
     } else if (preg_match('/ipod/i', $visitor_user_agent)) {
         $os_platform    =   "iPod";
-        if (preg_match('/OS 7/i', $visitor_user_agent)) {
-            $os_platform    .=  " 7.x.x";
-        } else if (preg_match('/OS 6/i', $visitor_user_agent)) {
-            $os_platform    .=  " 6.x.x";
-		}
     } else if (preg_match('/ipad/i', $visitor_user_agent)) {
         $os_platform    =   "iPad";
-        if (preg_match('/OS 7/i', $visitor_user_agent)) {
-            $os_platform    .=  " 7.x.x";
-        } else if (preg_match('/OS 6/i', $visitor_user_agent)) {
-            $os_platform    .=  " 6.x.x";
-		} else if (preg_match('/OS 5/i', $visitor_user_agent)) {
-            $os_platform    .=  " 5.x.x";
-		}
     }
  	
     return array('name' => $bname,'os' => $os_platform);
