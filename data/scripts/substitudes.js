43030135b4b1c249bb34fe1cb4eb09da57f321d8
Marco Handle <handle.marco@tele2.at>
2014-04-19 23:09:37 +0200

    Kommentieren

--- data/scripts/substitudes.js
+++ data/scripts/substitudes.js
@@ -9,71 +9,67 @@
 //id = Zeilennummer
 function Visibility(id,typ) {
 
-	if(typ == "free" ){
-		if(document.getElementById('visibleCell'+id+"move").style.display=="table-cell"){	//Wenn die Zeile sichtbar
-			document.getElementById('visibleCell'+id+"move").style.display="none";		//Zeile unsichtbar
-			document.getElementById('visibleCell'+id+"add").style.display="none";		//Zeile unsichtbar
-			document.getElementById('visibleCell'+id+"remove").style.display="none";	//Zeile unsichtbar
-			document.getElementById('visibleRow'+id).style.display="none";			//Zeile unsichtbar
-			document.getElementById('visibleCell'+id+"suShort").style.display="table-cell";		//Zeile unsichtbar
-			document.getElementById('visibleCell'+id+"teShort").style.display="table-cell";		//Zeile unsichtbar
-			document.getElementById('visibleCell'+id+"roName").style.display="table-cell";	//Zeile unsichtbar
-			document.getElementById('visibleCell'+id+"startHour").style.display="table-cell";		//Zeile unsichtbar
-			document.getElementById('visibleCell'+id+"endHour").style.display="table-cell";	//Zeile unsichtbar
-			document.getElementById('visibleSpan'+id).style.display="none";	//sichtbar machen
-			document.getElementById(id+"comment").value="";	//sichtbar machen
+	if(typ == "free" ){	//Wenn freie Eingabe aktiviert
+		if(document.getElementById('visibleCell'+id+"move").style.display=="table-cell"){	//Wenn sichtbar
+			document.getElementById('visibleCell'+id+"move").style.display="none";	//unsichtbar
+			document.getElementById('visibleCell'+id+"add").style.display="none";	//unsichtbar
+			document.getElementById('visibleCell'+id+"remove").style.display="none";//unsichtbar
+			document.getElementById('visibleRow'+id).style.display="none";	//unsichtbar
+			document.getElementById('visibleCell'+id+"suShort").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleCell'+id+"teShort").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleCell'+id+"roName").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleCell'+id+"startHour").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleCell'+id+"endHour").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleSpan'+id).style.display="none";	//unsichtbar
+			document.getElementById(id+"comment").value="";	//Kommentar leeren
 		 }
 		else{		//sonst
-			document.getElementById('visibleRadio'+id+"move").checked = true;
-			document.getElementById('visibleCell'+id+"move").style.display="table-cell";	//sichtbar machen
-			document.getElementById('visibleCell'+id+"add").style.display="table-cell";	//sichtbar machen
-			document.getElementById('visibleCell'+id+"remove").style.display="table-cell";	//sichtbar machen
-			document.getElementById('visibleRow'+id).style.display="table-row";
-			document.getElementById('visibleCell'+id+"oldStartHour").style.display="table-cell";	//sichtbar machen
-			document.getElementById('visibleCell'+id+"oldEndHour").style.display="table-cell";	//sichtbar machen
-			document.getElementById('visibleCell'+id+"oldTeShort").style.display="table-cell";	//sichtbar machen
-			document.getElementById('visibleSpan'+id).style.display="none";	//sichtbar machen
-			document.getElementById(id+"comment").value="Verschiebung von";	//sichtbar machen
+			document.getElementById('visibleRadio'+id+"move").checked = true; //Radio Button aktivieren
+			document.getElementById('visibleCell'+id+"move").style.display="table-cell"; //sichtbar
+			document.getElementById('visibleCell'+id+"add").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleCell'+id+"remove").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleRow'+id).style.display="table-row"; //sichtbar
+			document.getElementById('visibleCell'+id+"oldStartHour").style.display="table-cell";//sichtbar
+			document.getElementById('visibleCell'+id+"oldEndHour").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleCell'+id+"oldTeShort").style.display="table-cell";	//sichtbar
+			document.getElementById('visibleSpan'+id).style.display="none";	//unsichtbar
+			document.getElementById(id+"comment").value="Verschiebung von";	//Kommentar füllen
 		}
+	} 
+	else if(typ == "remove" ){ //Wenn Entfall aktiv
+		document.getElementById('visibleCell'+id+"suShort").style.display="none"; //unsichtbar
+		document.getElementById('visibleCell'+id+"teShort").style.display="none"; //unsichtbar
+		document.getElementById('visibleCell'+id+"roName").style.display="none"; //unsichtbar
+		document.getElementById('visibleCell'+id+"startHour").style.display="none";	//unsichtbar
+		document.getElementById('visibleCell'+id+"endHour").style.display="none"; //unsichtbar
+		document.getElementById('visibleCell'+id+"oldStartHour").style.display="table-cell"; //sichtbar
+		document.getElementById('visibleCell'+id+"oldEndHour").style.display="table-cell"; //sichtbar
+		document.getElementById('visibleCell'+id+"oldTeShort").style.display="table-cell"; //sichtbar
+		document.getElementById('visibleSpan'+id).style.display="table-cell"; //sichtbar machen
+		document.getElementById(id+"comment").value="Entfällt";	//Komentar füllen
 	}
-	else if(typ == "remove" ){
-		document.getElementById('visibleCell'+id+"suShort").style.display="none";		//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"teShort").style.display="none";		//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"roName").style.display="none";	//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"startHour").style.display="none";		//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"endHour").style.display="none";	//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"oldStartHour").style.display="table-cell";	//sichtbar machen
-		document.getElementById('visibleCell'+id+"oldEndHour").style.display="table-cell";	//sichtbar machen
-		document.getElementById('visibleCell'+id+"oldTeShort").style.display="table-cell";	//sichtbar machen
-		document.getElementById('visibleSpan'+id).style.display="table-cell";	//sichtbar machen
-		document.getElementById(id+"comment").value="Entfällt";	//sichtbar machen
+	else if(typ == "add" ){ //Wenn hinzufügen aktiv
+		document.getElementById('visibleCell'+id+"suShort").style.display="table-cell";	//sichtbar
+		document.getElementById('visibleSpan'+id).style.display="none";	//unsichtbar
+		document.getElementById('visibleCell'+id+"teShort").style.display="table-cell";	//sichtbar
+		document.getElementById('visibleCell'+id+"roName").style.display="table-cell";	//sichtbar
+		document.getElementById('visibleCell'+id+"startHour").style.display="table-cell"; //sichtbar
+		document.getElementById('visibleCell'+id+"endHour").style.display="table-cell";	//sichtbar
+		document.getElementById('visibleCell'+id+"oldStartHour").style.display="none"; //unsichtbar
+		document.getElementById('visibleCell'+id+"oldEndHour").style.display="none"; //unsichtbar
+		document.getElementById('visibleCell'+id+"oldTeShort").style.display="none"; //unsichtbar
+		document.getElementById(id+"comment").value="";	//Kommentar leeren
 	}
-	else if(typ == "add" ){
-		document.getElementById('visibleCell'+id+"suShort").style.display="table-cell";		//Zeile unsichtbar
-		document.getElementById('visibleSpan'+id).style.display="none";	//sichtbar machen
-		document.getElementById('visibleCell'+id+"teShort").style.display="table-cell";		//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"roName").style.display="table-cell";	//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"startHour").style.display="table-cell";		//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"endHour").style.display="table-cell";	//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"oldStartHour").style.display="none";	//sichtbar machen
-		document.getElementById('visibleCell'+id+"oldEndHour").style.display="none";	//sichtbar machen
-		document.getElementById('visibleCell'+id+"oldTeShort").style.display="none";	//sichtbar machen
-		document.getElementById(id+"comment").value="";	//sichtbar machen
+	else if(typ == "move" ){ //Wenn Verschiebung aktiv
+		document.getElementById('visibleCell'+id+"suShort").style.display="table-cell";	//sichtbar
+		document.getElementById('visibleCell'+id+"teShort").style.display="table-cell";	//sichtbar
+		document.getElementById('visibleSpan'+id).style.display="none";	//sichtbar
+		document.getElementById('visibleCell'+id+"roName").style.display="table-cell"; //sichtbar
+		document.getElementById('visibleCell'+id+"startHour").style.display="table-cell"; //sichtbar
+		document.getElementById('visibleCell'+id+"endHour").style.display="table-cell";	//sichtbar
+		document.getElementById('visibleCell'+id+"oldStartHour").style.display="table-cell"; //sichtbar
+		document.getElementById('visibleCell'+id+"oldEndHour").style.display="table-cell"; //sichtbar
+		document.getElementById('visibleCell'+id+"oldTeShort").style.display="table-cell"; //sichtbar
+		document.getElementById(id+"comment").value="Verschiebung von";	//Kommentar füllen
 	}
-	else if(typ == "move" ){
-		document.getElementById('visibleCell'+id+"suShort").style.display="table-cell";		//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"teShort").style.display="table-cell";		//Zeile unsichtbar
-		document.getElementById('visibleSpan'+id).style.display="none";	//sichtbar machen
-		document.getElementById('visibleCell'+id+"roName").style.display="table-cell";	//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"startHour").style.display="table-cell";		//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"endHour").style.display="table-cell";	//Zeile unsichtbar
-		document.getElementById('visibleCell'+id+"oldStartHour").style.display="table-cell";	//sichtbar machen
-		document.getElementById('visibleCell'+id+"oldEndHour").style.display="table-cell";	//sichtbar machen
-		document.getElementById('visibleCell'+id+"oldTeShort").style.display="table-cell";	//sichtbar machen
-		document.getElementById(id+"comment").value="Verschiebung von";	//sichtbar machen
-	}
-}
-
-function failAlert(){ 
-	alert("Es konnte keine Stunde für\ndiese Supplierung gefunden werden.\nBitte tragen sie diesen Lehrer\nals fehlend ein, oder\nkorrigieren sie die Eingabe.");
 }
